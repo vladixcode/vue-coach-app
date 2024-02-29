ef715d86da20ad7e249355a7c0cccc7513c648d0
Vladimir Ilic <32035850+vladixcode@users.noreply.github.com>
2024-02-29 14:59:27 +0100

    Vuex - Setup Vuex; dummy global state data; Coach List item component (#2)

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import './style.css'
 
 import App from './App.vue'
 import router from './router'
+import store from './store/index'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(store)
 
 app.mount('#app')
--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -5,6 +5,47 @@
       <button>Refresh</button>
       <router-link to="/register">Register as a Coach</router-link>
     </div>
-    Coaches list
+    <ul v-if="hasCoaches">
+      <coach-item
+        v-for="coach in filteredCoaches"
+        :id="coach.id"
+        :key="coach.id"
+        :first-name="coach.firstName"
+        :last-name="coach.lastName"
+        :areas="coach.areas"
+        :rate="coach.hourlyRate"
+      ></coach-item>
+    </ul>
+    <h3 v-else>No coaches found</h3>
   </section>
 </template>
+
+<script>
+import CoachItem from '@/components/coaches/CoachItem.vue'
+export default {
+  components: {
+    CoachItem,
+  },
+  computed: {
+    filteredCoaches() {
+      return this.$store.getters['coaches/coaches']
+    },
+    hasCoaches() {
+      return this.$store.getters['coaches/hasCoaches']
+    },
+  },
+}
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
