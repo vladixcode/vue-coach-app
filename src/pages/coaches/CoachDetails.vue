6c968ff52e9d3e87d2ad92e6a5b23d1f0848e27d
Vladimir Ilic <32035850+vladixcode@users.noreply.github.com>
2024-02-29 16:59:29 +0100

    Coach Details - Building Coach Details Component (#4)

--- src/pages/coaches/CoachDetails.vue
+++ src/pages/coaches/CoachDetails.vue
@@ -1,5 +1,52 @@
 <template>
-  <h1>Coach Deatils</h1>
-  <router-view></router-view>
-  <router-link to="/coaches/c1/contact">Contact</router-link>
+  <section>
+    <base-card>
+      <h2>{{ fullName }}</h2>
+      <h3>${{ selectedCoach.hourlyRate }}/hour</h3>
+    </base-card>
+  </section>
+
+  <section>
+    <base-card>
+      <header>
+        <h2>Interested? Reach out now</h2>
+        <base-button link :to="contactLink">Contact</base-button>
+      </header>
+      <router-view></router-view>
+    </base-card>
+  </section>
+
+  <section>
+    <base-card>
+      <base-badge v-for="area in selectedCoach.areas" :key="area" :type="area" :title="area"></base-badge>
+      <p>{{ selectedCoach.description }}</p>
+    </base-card>
+  </section>
 </template>
+
+<script>
+export default {
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      selectedCoach: null,
+    }
+  },
+  computed: {
+    fullName() {
+      return this.selectedCoach.firstName + ' ' + this.selectedCoach.lastName
+    },
+    contactLink() {
+      return `${this.$route.path}/${this.id}/contact`
+    },
+  },
+  created() {
+    this.selectedCoach = this.$store.getters['coaches/coaches'].find((coach) => coach.id === this.id)
+  },
+}
+</script>
