1b6026350105d22cf620a3703305513675145361
Vladimir Ilic <vladimir.ilic101@gmail.com>
2024-02-29 16:33:46 +0100

    Base components - Create base/common/shared components and register them globally

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,6 +8,7 @@ module.exports = {
   extends: ['eslint:recommended', 'plugin:vue/vue3-recommended', '@vue/eslint-config-prettier'],
   rules: {
     // override/add rules settings here, such as:
+    'vue/component-definition-name-casing': ['error', 'kebab-case'],
     // 'vue/no-unused-vars': 'error'
     // 'vue/component-name-in-template-casing': ['error', 'PascalCase'],
   },
