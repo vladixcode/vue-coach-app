a9f5975182d26b810300f899c5c6b5522b1ca836
Vladimir Ilic <vladimir.ilic101@gmail.com>
2024-03-05 15:10:13 +0100

    Not Found page - Implement Not Found page for all non-supported routes

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,3 +1,8 @@
 <template>
-  <h1>Page Not Found</h1>
+  <section>
+    <base-card>
+      <h2>Page not found</h2>
+      <p>This page could not be found. Why not check all our <router-link to="/coaches">coaches</router-link>?</p>
+    </base-card>
+  </section>
 </template>
