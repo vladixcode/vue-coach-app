50d7a6c32e0ca72daa638f542b6a573c921beb96
Vladimir Ilic <32035850+vladixcode@users.noreply.github.com>
2024-03-12 10:31:19 +0100

    Optimization - Components that are not always needed can be lazy loaded; asynchronous components (#27)

--- src/router.js
+++ src/router.js
@@ -1,13 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import CoachesList from '@/pages/coaches/CoachesList.vue'
-import CoachDetails from './pages/coaches/CoachDetails.vue'
-import CoachRegistration from './pages/coaches/CoachRegistration.vue'
-import ContactCoach from './pages/requests/ContactCoach.vue'
-import RequestsReceived from './pages/requests/RequestsReceived.vue'
-import UserAuth from './pages/auth/UserAuth.vue'
 import NotFound from './pages/NotFound.vue'
 import store from './store/index'
+
+const CoachDetails = () => import('./pages/coaches/CoachDetails.vue')
+const CoachRegistration = () => import('./pages/coaches/CoachRegistration.vue')
+const ContactCoach = () => import('./pages/requests/ContactCoach.vue')
+const RequestsReceived = () => import('./pages/requests/RequestsReceived.vue')
+const UserAuth = () => import('./pages/auth/UserAuth.vue')
 
 const router = createRouter({
   history: createWebHistory(),
